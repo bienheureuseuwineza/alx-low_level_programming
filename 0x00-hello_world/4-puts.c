ae8683557b164588bba13219b7c886f7d3b3abd3
Uwineza Bienheureuse <bienheureuseuwineza@gmail.com>
2022-03-13 08:07:02 -0700

    the following message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 /**
  *main - entry point
  *
@@ -7,5 +7,5 @@
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+reuturn (0);
 }
